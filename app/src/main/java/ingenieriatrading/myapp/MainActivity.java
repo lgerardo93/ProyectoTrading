7baa925a24318409ab81f8d1088a73e03efc5c54
YairRdz <riay-@hotmail.com>
2017-03-06 11:30:07 -0600

    Agrgue la vista del mensaje en la ventana principal

--- app/src/main/java/ingenieriatrading/myapp/MainActivity.java
+++ app/src/main/java/ingenieriatrading/myapp/MainActivity.java
@@ -2,14 +2,25 @@ package ingenieriatrading.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView infoTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoTextView = (TextView) findViewById(R.id.infotv);
+        if(getIntent().getExtras() != null){
+            for(String key : getIntent().getExtras().keySet()){
+                String value = getIntent().getExtras().getString(key);
+                infoTextView.append("\n" + key + ": " + value);
+            }
+        }
     }
 }
